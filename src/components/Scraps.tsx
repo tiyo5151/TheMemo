3284bc137a8fb6e5c1323586db9499a2817316dd
tiyo5151 <s1F102400224@iniad.org>
2024-10-04 23:08:26 +0900

    見た目

--- src/components/Scraps.tsx
+++ src/components/Scraps.tsx
@@ -2,7 +2,27 @@ import React from 'react';
 import '../styles/Scraps.css';
 
 const Scraps = () => {
-  return <div className="app-scraps">Scraps</div>;
+  return (
+    <div className="app-scraps">
+      <div className="app-scrap-header">
+        Sample
+        <div className="google-icon"></div>
+      </div>
+      <div className="app-scrap-content">
+        This is a sample scrapaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      </div>
+      <div className="app-scrap-input">
+        <div className="input-header">
+          sadsadsads
+          <div className="google-icon-input"></div>
+        </div>
+        <textarea placeholder="Enter your memo"></textarea>
+        <div className="input-footer">
+          <button>追加</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Scraps;
